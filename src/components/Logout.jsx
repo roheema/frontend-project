092586d469b4318a36fbd1e26784ff94a5512ac4
fiyinfoluwa17 <adegokeoyesanya305@gmail.com>
2023-11-16 15:03:06 +0100

    final

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import CustomLoader from '../components/Loader';
+import './Signup.css'
+
 
 const Logout = () => {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const logout = async () => {
-      // Simulate an asynchronous operation, like making an API call for logout
-      // You can replace this with your actual logout logic
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+    // Clear the authentication token (or session)
+    localStorage.removeItem("authToken"); // Adjust this according to how you store the token
 
-      // Clear the authentication token (or session)
-      localStorage.removeItem("authToken"); // Adjust this according to how you store the token
-
-      // Redirect to the home page
-      navigate("/");
-    };
-
-    logout().finally(() => setLoading(false));
+    // Redirect to the home page
+    navigate("/");
   }, [navigate]);
 
-  return (
-    <div>
-      {loading ? (
-        <CustomLoader /> // Replace with your loader component
-      ) : (
-        <div>Logout complete!</div>
-      )}
-    </div>
-  );
+  return <div className="logout"><h1>Logging Out...</h1></div>
 };
 
 export default Logout;
